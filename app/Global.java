c7502579b395b44fcfed8d8ed8eaf6c032863d6b
Alvin Prieto <aj.garidanprieto@gmail.com>
2013-11-01 11:19:30 -1000

    not sure why it's not working anymore

--- app/Global.java
+++ app/Global.java
@@ -6,8 +6,8 @@ import play.GlobalSettings;
 public class Global extends GlobalSettings {
 
   public void onStart(Application app) {
-    CreateInventory.createSnacksInventory();
-    CreateInventory.createBeveragesInventory();
+    //CreateInventory.createSnacksInventory();
+    //CreateInventory.createBeveragesInventory();
     CreateInventory.createToysInventory();
     CreateInventory.createCigarInventory();
   }
